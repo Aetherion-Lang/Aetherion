a2efd4196e3e13c23aef945794e66dccd2a11d19
Placidity <69digital.contact@gmail.com>
2024-05-28 21:54:34 +0000

    lexer pt. 2

--- lexer.ts
+++ lexer.ts
@@ -6,9 +6,13 @@ export enum TokenType{
     Identifier,
     Equals,
     OpenParen,
-    ClosePare,
+    CloseParen,
     BinaryOperator,
     Let,
+}
+
+const KEYWORDS: Record<string, TokenType> = {
+    "let": TokenType.Let,
 }
 
 export interface Token {
@@ -20,16 +24,59 @@ function token (value = "", type: TokenType): Token {
     return { value, type };
 }
 
+function isalpha (src: string) {
+    return src.toUpperCase() !=  src.toLowerCase();
+}
+
+function isint (str: string) {
+    const c = str.charCodeAt(0);
+    const bounds = ['0'.charCodeAt(0), '9'.charCodeAt(0)];
+    return (c >= bounds[0] && c <= bounds[1]);
+}
+
 export function tokenize(sourceCode: string): Token[] {
     const tokens = new Array<Token>();
     const src = sourceCode.split("")
 
     // Build each token until end of file
     while (src.length > 0) {
-        if (src[0] == '(') {
+        if (src[0] == "(") {
             tokens.push(token(src.shift(), TokenType.OpenParen));
-        } else if (src[0] == "") {
-            tokens.push(token(src.shift(), TokenType.OpenParen));
+        } else if (src[0] == ")") {
+            tokens.push(token(src.shift(), TokenType.CloseParen));
+        } else if (
+          src[0] == "+" || src[0] == "-" || src[0] == "*" || src[0] == "/"
+        ) {
+            tokens.push(token(src.shift(), TokenType.BinaryOperator));
+        } else if (src[0] == "=") {
+            tokens.push(token(src.shift(), TokenType.Equals));
+        } else {
+            // Handle multicharacter tokens
+            
+            // Build number token
+            if (isint(src[0])) {
+                let num = "";
+                while (src.length > 0 && isint(src[0])) {
+                    num += src.shift();
+                }
+
+                tokens.push(token(num, TokenType.Number));
+
+            } else if (isalpha(src[0])) {
+                let ident = ""; // foo Let
+                while (src.length > 0 && isalpha(src[0])) {
+                    ident += src.shift();
+                }
+
+                // check for reserved keywords
+                const reserved = KEYWORDS[ident];
+
+                if (reserved == undefined ) {
+                    tokens.push(token(ident, TokenType.Identifier));
+                } else {
+                    tokens.push(token(ident, reserved));
+                }
+            }
         }
     }
 
