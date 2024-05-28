885d0c084e103972f7f351a40ac87390a1816c7e
Placidity <69digital.contact@gmail.com>
2024-05-28 21:12:09 +0000

    started lexer

---
+++ lexer.ts
@@ -0,0 +1,37 @@
+// Let x = 45;
+// [ LetToken, IdentifierTk, EqualsToken, NumberToken ]
+
+export enum TokenType{
+    Number,
+    Identifier,
+    Equals,
+    OpenParen,
+    ClosePare,
+    BinaryOperator,
+    Let,
+}
+
+export interface Token {
+    value: string,
+    type: TokenType,
+}
+
+function token (value = "", type: TokenType): Token {
+    return { value, type };
+}
+
+export function tokenize(sourceCode: string): Token[] {
+    const tokens = new Array<Token>();
+    const src = sourceCode.split("")
+
+    // Build each token until end of file
+    while (src.length > 0) {
+        if (src[0] == '(') {
+            tokens.push(token(src.shift(), TokenType.OpenParen));
+        } else if (src[0] == "") {
+            tokens.push(token(src.shift(), TokenType.OpenParen));
+        }
+    }
+
+    return tokens;
+}
